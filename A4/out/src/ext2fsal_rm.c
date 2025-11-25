7ab7534b262d2fdb0406dc3769d16c73a4368320
francoxortiz1975 <francoxortiz2005@gmail.com>
2025-11-25 15:45:56 -0500

    rm 1st implementation

--- A4/out/src/ext2fsal_rm.c
+++ A4/out/src/ext2fsal_rm.c
@@ -20,15 +20,42 @@
 #include <errno.h>
 
 
-int32_t ext2_fsal_rm(const char *path)
-{
+int32_t ext2_fsal_rm(const char *path){
     /**
      * TODO: implement the ext2_rm command here ...
      * the argument 'path' is the path to the file to be removed.
      */
 
-     /* This is just to avoid compilation warnings, remove this line when you're done. */
-    (void)path;
+    //path walk
+    struct ex2_dir_wrapper result = e2_path_walk_absolute(path);
+    
+    //if not exits, error
+    if (result.errcode != 0) {
+        return -ENOENT;
+    }
+    
+    //if not directory, error
+    if (result.entry->file_type == EXT2_FT_DIR) {
+        return -EISDIR;
+    }
+    
+    //get inode
+    struct ext2_inode *file_inode = resolve_inode_number(result.entry->inode);
+    //decreaser file link count
+    file_inode->i_links_count--;
+    
+    // if no more links, clean.
+    if (file_inode->i_links_count == 0) {
+        //free data blocks
+        int blocks_used = (file_inode->i_size + 1023) / 1024;
+        free_blocks(file_inode, 0, blocks_used);  // free all blocks
+        
+        //free inode
+        ex2_unmark_inode_bitmap(result.entry->inode);
+    }
+    
+    // mark as free directory
+    ex2_free_dir_entry(result.entry);
 
     return 0;
 }
